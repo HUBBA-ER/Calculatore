843979914473d4edf2e50462ebc87150f4d9617a
KabirBhatia <kabirbhatia04@gmail.com>
2015-11-05 12:47:21 -0800

    Update Calculatore.js

--- Calculatore.js
+++ Calculatore.js
@@ -1,2 +1,3 @@
 var Op = prompt("Welcome to the Calculatore! (It's a calculator) Enter an operation, ADD, SUBTRACT, MULTIPLY or DIVIDE:")
-var no1 = prompt("Please enter one of the numbers to" + Op + " ")
+var no1 = prompt("Please enter one of the numbers to" + Op + ":")
+var no2 = prompt("Please enter another number to" + Op + ":")
