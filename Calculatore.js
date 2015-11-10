5bf3f82e8224ad0c73b98153fc8d281f059da32c
KabirBhatia <kabirbhatia04@gmail.com>
2015-11-10 13:03:03 -0800

    Update Calculatore.js
    update and work

--- Calculatore.js
+++ Calculatore.js
@@ -1,3 +1,26 @@
-var Op = prompt("Welcome to the Calculatore! (It's a calculator) Enter an operation, ADD, SUBTRACT, MULTIPLY or DIVIDE:")
-var no1 = prompt("Please enter one of the numbers to" + Op + ":")
-var no2 = prompt("Please enter another number to" + Op + ":")
+var Op = prompt("Welcome to the Calculatore! (It's a calculator) Enter an operation, ADD, SUBTRACT, MULTIPLY or DIVIDE:");
+var no1 = prompt("Please enter one of the numbers to " + Op + ":");
+var no2 = prompt("Please enter another number to " + Op + ":");
+var calc = function() {
+  //adding
+  if (Op == "ADD") {
+    var a = no1 + no2
+    document.write("The sum is" + " :" + a)
+  }
+  //subtracting
+  else if (Op == "SUBTRACT") {
+    var a = no1 - no2
+    document.write("The difference is" + " :" + a)
+  }
+  //multiplying
+  else if (Op == "MULTIPLY") {
+    var a = no1 * no2
+    document.write("The product is" + " :" + a)
+  }
+  //dividing
+  else if (Op == "DIVIDE") {
+    var a = no1 / no2
+    document.write("The quotient is" + " :" + a)
+  }
+}
+calc()
