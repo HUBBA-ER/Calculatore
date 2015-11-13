f7e2e00eea6745f26e05b5e149c1673d2eb7bde4
KabirBhatia <kabirbhatia04@gmail.com>
2015-11-13 12:48:07 -0800

    Update Calculatore.js

--- Calculatore.js
+++ Calculatore.js
@@ -1,6 +1,6 @@
 var Op = prompt("Welcome to the Calculatore! (It's a calculator) Enter an operation, ADD, SUBTRACT, MULTIPLY or DIVIDE:");
-var no1 = prompt("Please enter one of the numbers to " + Op + ":");
-var no2 = prompt("Please enter another number to " + Op + ":");
+var no1 = parseInt(prompt("Please enter one of the numbers to " + Op + ":"));
+var no2 = parseInt(prompt("Please enter another number to " + Op + ":"));
 var calc = function() {
   //adding
   if (Op == "ADD") {
