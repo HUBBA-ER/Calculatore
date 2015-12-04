904d97d3ff97a4bd03f75c9260a61a3c89b29b4b
Tripp Lyons <tripplyons@gmail.com>
2015-12-04 12:31:31 -0800

    Update Calculatore.js

--- Calculatore.js
+++ Calculatore.js
@@ -4,23 +4,25 @@ var no2 = parseInt(prompt("Please enter another number to " + Op + ":"));
 var calc = function() {
   //adding
   if (Op == "ADD") {
-    var a = no1 + no2
-    document.write("The sum is" + " :" + a.toString())
+    var a = no1 + no2;
+    document.write("The sum is" + ": " + a.toString());
   }
   //subtracting
   else if (Op == "SUBTRACT") {
-    var a = no1 - no2
-    document.write("The difference is" + " :" + a.toString())
+    var a = no1 - no2;
+    document.write("The difference is" + ": " + a.toString());
   }
   //multiplying
   else if (Op == "MULTIPLY") {
-    var a = no1 * no2
-    document.write("The product is" + " :" + a.toString())
+    var a = no1 * no2;
+    document.write("The product is" + ": " + a.toString());
   }
   //dividing
   else if (Op == "DIVIDE") {
-    var a = no1 / no2
-    document.write("The quotient is" + " :" + a.toString())
+    var a = no1 / no2;
+    document.write("The quotient is" + ": " + a.toString());
   }
 }
+
+
 calc()
